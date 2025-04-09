2219224137a2a545581cc3a4f5af55e82aa84b66
Titus Silver <titus.silver.ph@gmail.com>
2025-04-10 05:16:26 +0800

    update code

--- app/Http/Controllers/TeacherGradingController.php
+++ app/Http/Controllers/TeacherGradingController.php
@@ -120,6 +120,19 @@ class TeacherGradingController extends Controller
                 }
             }
 
+            dd([
+                'students' => $allStudents,
+                'studentAss' => $studentAss,
+                'submissions' => $studentSub,
+                'grades' => $grades,
+                'workBehavior' => $workBehavior,
+                'socialSkills' => $socialSkills,
+                'cognitiveSkills' => $cognitiveSkills,
+                'fms' => $fms,
+                'gms' => $gms,
+                'adls' => $adls,
+            ]);
+
             return view('teacher.grading', [
                 'students' => $allStudents,
                 'studentAss' => $studentAss,
