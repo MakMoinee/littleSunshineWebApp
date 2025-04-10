b828393926b2eb0042db2108689ee95da361d43f
Titus Silver <titus.silver.ph@gmail.com>
2025-04-11 00:46:39 +0800

    update code

--- app/Http/Controllers/TeacherGradingController.php
+++ app/Http/Controllers/TeacherGradingController.php
@@ -121,8 +121,6 @@ class TeacherGradingController extends Controller
                     $adls[$s['id']] = json_decode($grade['adls'], true);
                 }
             }
-
-
             return view('teacher.grading', [
                 'students' => $allStudents,
                 'studentAss' => $studentAss,
