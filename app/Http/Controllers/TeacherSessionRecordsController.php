b654e52d6e5b61feb3af27a7463562842bd57b00
Titus Silver <titus.silver.ph@gmail.com>
2025-04-18 15:50:37 +0800

    update code

--- app/Http/Controllers/TeacherSessionRecordsController.php
+++ app/Http/Controllers/TeacherSessionRecordsController.php
@@ -27,7 +27,7 @@ class TeacherSessionRecordsController extends Controller
             if ($search) {
                 $allSessions = DB::table('vwsessions')
                     ->where('teacherID', '=', $user['userID'])
-                    ->where('name', '=', $search)
+                    ->where('name', 'LIKE', "%" . $search . "%")
                     ->orderBy('created_at', 'desc')
                     ->paginate(10);
             } else {
